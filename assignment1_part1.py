7df4b2545d78457096b87e5afacc24d22e3615e9
Marcus199825 <157505839+Marcus199825@users.noreply.github.com>
2024-01-28 17:50:29 -0500

    Python Assignments 1 & 2 Completed

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,19 +1,41 @@
 
-def list_divide(numbers, divide):
+def list_divide(num, divide=2):
+    divisibleNumbers = []
+    for i in num:
+        if(i % divide == 0):
+            divisibleNumbers.append(i)
+
+    return len(divisibleNumbers)
     """
-    The function returns the number of elements in the numbers list that are divisibleby divide
+    The function returns the number of elements in the numbers list that are divisible by 2
     """
-    pass
+
+class listDivideException:
+
+    def __init__(self):
+        raise Exception("Division Error")
+
+
+
+def list_divide(num, divide=2):
+    divisibleNumbers = []
+    for i in num:
+        if(i % divide == 0):
+            divisibleNumbers.append(i)
+
+    return len(divisibleNumbers)      
+
 
 def test_list_divide():
-    """
-    Test listDivide
-    """
-    assert listDivide([1,2,3,4,5]) == 2
-    assert listDivide([2,4,6,8,10]) == 5
-    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
-    assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 5
+
+    try:
+        assert list_divide([1,2,3,4,5]) == 2
+        assert list_divide([2,4,6,8,10]) == 5
+        assert list_divide([30,54,63,98,100], 10) == 2
+        assert list_divide([]) == 0
+        assert list_divide([1,2,3,4,5], 1) == 5
+
+    except:
+        listDivideException()
     
-if __name__ == "__main__":
-    testListDivide()
+    test_list_divide()
--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,14 @@
 
 class Book:
     def __init__(self, author, title):
-        pass
+        self.author = author
+        self.title = title
     
     def display(self):
-        pass
+        print(self.title + " by " + self.author)
 
+book1 = Book("J.K Rowling", "Harry Potter and the Goblet of Fire")        
+book2 = Book("Walter Scott", "Ivanhoe")
 
-if __name__ == "__main__":
-    pass
+book1.display()
+book2.display()
